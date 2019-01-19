5740b32de1fc68a40b478c7763e42a02f1f7a8cd
zozo <anhnguyen6281@gmail.com>
2019-01-19 08:51:18 +0200

    add string example

---
+++ llvm_byte_code/test/string.tig.s
@@ -0,0 +1,28 @@
+	.text
+	.file	"Tiger jit"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entry
+	subq	$24, %rsp
+	.cfi_def_cfa_offset 32
+	movq	$.L__unnamed_1, 8(%rsp)
+	movl	$.L__unnamed_1, %edi
+	callq	tig_print
+	xorl	%eax, %eax
+	addq	$24, %rsp
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L__unnamed_1,@object   # @0
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L__unnamed_1:
+	.asciz	"hello\nworld"
+	.size	.L__unnamed_1, 12
+
+
+	.section	".note.GNU-stack","",@progbits

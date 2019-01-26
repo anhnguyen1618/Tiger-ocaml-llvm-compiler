c57b7965ea9e4245f6785ef27a71e1822ff153ff
zozo <anhnguyen6281@gmail.com>
2019-01-26 20:20:34 +0200

    remove function call name

--- llvm_byte_code/test/test.tig.s
+++ llvm_byte_code/test/test.tig.s
@@ -6,15 +6,22 @@
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:                                # %entry
-	pushq	%rax
+	pushq	%rbx
 	.cfi_def_cfa_offset 16
-	movq	%rsp, %rdi
+	subq	$16, %rsp
+	.cfi_def_cfa_offset 32
+	.cfi_offset %rbx, -16
+	leaq	8(%rsp), %rbx
 	movl	$30, %esi
+	movq	%rbx, %rdi
 	callq	fib
 	movl	%eax, %edi
 	callq	tig_print_int
+	movq	%rbx, %rdi
+	callq	a
 	xorl	%eax, %eax
-	popq	%rcx
+	addq	$16, %rsp
+	popq	%rbx
 	retq
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
@@ -66,6 +73,18 @@ fib:                                    # @fib
 	retq
 .Lfunc_end1:
 	.size	fib, .Lfunc_end1-fib
+	.cfi_endproc
+                                        # -- End function
+	.globl	a                       # -- Begin function a
+	.p2align	4, 0x90
+	.type	a,@function
+a:                                      # @a
+	.cfi_startproc
+# %bb.0:                                # %entry
+	movq	%rdi, -8(%rsp)
+	retq
+.Lfunc_end2:
+	.size	a, .Lfunc_end2-a
 	.cfi_endproc
                                         # -- End function
 
